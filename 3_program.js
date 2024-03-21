c30ac39fcd87f72117678e0c69ffe70746e782a9
AhmedShakeel110 <164321009+AhmedShakeel110@users.noreply.github.com>
2024-03-21 14:05:11 +0500

    Delete 3_program.js

--- 3_program.js
+++
@@ -1,4 +0,0 @@
-var person_name = "Ahmed Shakeel";
-console.log(person_name.toLowerCase());
-console.log(person_name.toUpperCase());
-console.log(person_name.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
